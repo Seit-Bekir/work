30f5c4989204fadc062bfa17c14c273d6af9c54f
Kamilov Seit-Bekir <kam-ceit-bekir@mail.ru>
2025-02-09 10:40:51 +0300

    task 1 ending

--- src/day1/task1.java
+++ src/day1/task1.java
@@ -1,4 +1,13 @@
 package day1;
 
 public class task1 {
+    public static void main(String[] args) {
+        int i=0;
+        while (true)
+        {
+            System.out.println("java");
+            i++;
+            if (i>=10){break;}
+        }
+    }
 }
