2b51e4d0667f5b9c057103edd5b3914ddbd31cd3
Kamilov Seit-Bekir <kam-ceit-bekir@mail.ru>
2025-02-09 12:26:20 +0300

    first task 2 day

---
+++ src/day2/Task1.java
@@ -0,0 +1,25 @@
+package day2;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите количество этажей:");
+        int florCount = scanner.nextInt();
+        if (florCount<=0){
+            System.out.println(" Не корректное количетво этажей");
+        }
+        else if(florCount>=1 && florCount<=4)
+        {
+            System.out.println("Малоэтажный дом");
+        }
+        else if(florCount>=4 && florCount<=8){
+            System.out.println("Среднеэтажный дом");
+        }
+        else if(florCount>8)
+        {
+            System.out.println("Многоэтажный дом");
+        }
+    }
+}
