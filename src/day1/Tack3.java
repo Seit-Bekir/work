b2922ff8eed90b40f73539d426fcae80df657f4d
Kamilov Seit-Bekir <kam-ceit-bekir@mail.ru>
2025-02-09 11:44:05 +0300

    ending day

---
+++ src/day1/Tack3.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class Tack3 {
+    public static void main(String[] args) {
+        int count = 0;
+        while(count<10)
+        {
+            System.out.println("JAVA");
+            count++;
+        }
+    }
+}
---
+++ src/day1/Tack5.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class Tack5 {
+    public static void main(String[] args) {
+        int age = 34;
+        for(int i = age;i<=100;i+=5) {
+            System.out.println("Мой возраст "+i);
+        }
+    }
+}
